54ac04d6ccbccca7c486611ed7fdb8f2730664ac
Jordan Gagnon <itsseraphii@gmail.com>
2024-11-18 23:32:21 -0500

    Restarted from scratch lmao

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h2>🤓🤓🤓🤓🤓🤓🤓🤓🤓 Get rotated nerd 🤓🤓🤓🤓🤓🤓🤓🤓🤓</h2>
+    <img style="align-self: center;" src="../assets/fish-spinning.gif" alt="A spinning fish" />
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings {
+  text-align: center;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/views/FirstView.vue
@@ -0,0 +1,5 @@
+<template>
+  <main style="display: flex; justify-content: center; padding: 48px 0;">
+    Cool stuff is yet to come!
+  </main>
+</template>
