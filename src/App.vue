a89224fcd3c5a70f5599500076f3c1951fb5cb13
Jordan Gagnon <itsseraphii@gmail.com>
2024-11-19 00:06:33 -0500

    Added some branding

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" style="background-color: white;" class="logo" src="./assets/logoCCI.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="C3I is on the internet" />
